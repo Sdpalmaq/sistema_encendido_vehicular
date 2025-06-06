ddbad1537e2daae499785df394b38cbfe5764373
Sdpalmaq <sdpalmaq@utn.edu.ec>
2025-06-06 06:50:08 -0500

    refactor: update API request timeout and enhance error handling in restaurarSistema controller

--- App_S_V/services/api.js
+++ App_S_V/services/api.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://tesis-backend-xro2.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
-  timeout: 5000, // Tiempo de espera para las solicitudes
+  timeout: 20000, // Tiempo de espera para las solicitudes
   headers: {
     "Content-Type": "application/json",
   },
